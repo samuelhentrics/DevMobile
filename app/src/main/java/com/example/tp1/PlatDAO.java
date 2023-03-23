185e119a4f1f1d4a8bf81588e87066313ec97120
Samuel HENTRICS <samuel.hentrics@gmail.com>
2023-03-23 11:18:07 +0100

    DAO

--- app/src/main/java/com/example/tp1/PlatDAO.java
+++ app/src/main/java/com/example/tp1/PlatDAO.java
@@ -2,51 +2,73 @@ package com.example.tp1;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 
 import java.util.ArrayList;
 
 public class PlatDAO {
-
     private static String base = "BDTodoList";
     private static int version = 1;
     private BdSQLiteOpenHelper accesBD;
 
     public PlatDAO(Context ct){
         accesBD = new BdSQLiteOpenHelper(ct, base, null, version);
+
     }
 
     public Plat getPlat(long idP){
-        Plat lePlat = null;
+        Plat laPlat = null;
         Cursor curseur;
-        curseur = accesBD.getReadableDatabase().rawQuery("select * from plat where idP="+idP+";",null);
+        curseur = accesBD.getReadableDatabase().rawQuery("select * from Plat where idP="+idP+";",null);
         if (curseur.getCount() > 0) {
             curseur.moveToFirst();
-            lePlat = new Plat(idP,curseur.getString(1),1);
+            laPlat = new Plat(idP,curseur.getString(1), curseur.getLong(2));
         }
-        return lePlat;
+        return laPlat;
+    }
+
+    public void addPlat(Plat unePlat){
+        Log.d("testLog","insert into Plat(libelleT,idC) values('"+unePlat.getLibelleP()+"',"+unePlat.getIdTP()+");");
+        accesBD.getWritableDatabase().execSQL("insert into Plat(libelleT,idC) values('"+unePlat.getLibelleP()+"',"+unePlat.getIdTP()+");");
+        accesBD.close();
+    }
+
+    public void delPlat(Plat unePlat){
+        Log.d("testLog","delete from Plat where idT="+unePlat.getIdP()+";");
+        accesBD.getWritableDatabase().execSQL("delete from Plat where idT="+unePlat.getIdP()+";");
+        accesBD.close();
     }
 
     public ArrayList<Plat> getPlats(){
         Cursor curseur;
-        String req = "select * from plat;";
+        String req = "select * from Plat;";
+        curseur = accesBD.getReadableDatabase().rawQuery(req,null);
+        return cursorToPlatArrayList(curseur);
+    }
+    public ArrayList<Plat> getPlatsByTypeplat(String nomTP){
+        Cursor curseur;
+        String req = "select * from Plat, categorie where Plat.idTP = categorie.idTP and nomTP='"+nomTP+"';";
         curseur = accesBD.getReadableDatabase().rawQuery(req,null);
         return cursorToPlatArrayList(curseur);
     }
 
+
+
     private ArrayList<Plat> cursorToPlatArrayList(Cursor curseur){
-        ArrayList<Plat> listePlat = new ArrayList<Plat>();
+        ArrayList<Plat> listePlats = new ArrayList<Plat>();
         long idP;
-        String libelleP;
+        String libelleT;
+        long idTP;
 
         curseur.moveToFirst();
         while (!curseur.isAfterLast()){
             idP = curseur.getLong(0);
-            libelleP = curseur.getString(1);
-            listePlat.add(new Plat(idP,libelleP,1));
+            libelleT = curseur.getString(1);
+            idTP = curseur.getLong(2);
+            listePlats.add(new Plat(idP,libelleT,idTP));
             curseur.moveToNext();
         }
-        return listePlat;
+
+        return listePlats;
     }
-
-
 }
